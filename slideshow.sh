83f6434774b9efd9d00b0ef9eecc28d79bfce7a8
Michael Jacobs <jacobs@cs.uni-saarland.de>
2017-06-06 15:45:11 +0200

    Enabled a more flexible configuration of the browser command by an environment variable.

--- slideshow.sh
+++ slideshow.sh
@@ -3,5 +3,11 @@
 # exit whenever something unexpected happens
 set -e
 
+# set the browser command to a fallback value in case
+# it is not yet set in the environment
+if [ -z "${SLIDESHOWBROWSER}" ]; then
+	SLIDESHOWBROWSER="firefox -new-window"
+fi
+
 # run slideshow in the browser
-firefox -new-window index.html&
+${SLIDESHOWBROWSER} index.html&
